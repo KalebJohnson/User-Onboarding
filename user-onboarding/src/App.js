14610305f5753a470cc2ed2253605da95fe0d3d7
unknown <KalebJohnson45@yahoo.com>
2020-05-22 16:52:12 -0500

    cypress work done

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -7,15 +7,6 @@ import Form from './Components/Form';
 function App() {
   const [form, setForm] = useState([])
 
-  const addNewForm = form => {
-    const newForm = {
-      id: form.length + 1,
-      name: form.name,
-      email: form.email,
-      password: form.password
-    };
-    setForm([...form, newForm]);
-  };
 
   return (
     <div className="App">
